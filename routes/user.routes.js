dfb000a0504309c2998fb1630e9616104dd976d1
kingwerd <edrobinson046@gmail.com>
2019-04-16 01:03:24 -0400

    Added some comments

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -9,7 +9,7 @@ const UserController = require('../controllers/user.controller');
 router.post('/signup', UserController.signup);
 router.post('/signin', UserController.signin);
 
-// This route uses verifyToken as middleware to authenticate a user
+// This route uses verifyToken as middleware to authorize a user
 router.post('/profile', verifyToken, UserController.profile);
 
 module.exports = router;
